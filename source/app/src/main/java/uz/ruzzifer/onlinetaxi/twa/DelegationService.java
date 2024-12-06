964acbe0e251be7653617e707e0b7afe52b6e156
strelok93 <strelok0493@gmail.com>
2024-12-06 11:02:54 +0500

    first commit

---
+++ source/app/src/main/java/uz/ruzzifer/onlinetaxi/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package uz.ruzzifer.onlinetaxi.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
